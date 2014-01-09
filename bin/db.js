7522fae6b461b962f1d00fe7c65dada6f1a4e079
Tane Piper <piper.tane@gmail.com>
2014-01-09 14:06:11 +0000

    Fixing auth issue

--- bin/db.js
+++ bin/db.js
@@ -3,7 +3,7 @@ var MongoClient = require('mongodb').MongoClient;
 module.exports = function(done) {
   
   var dev = 'mongodb://127.0.0.1:27017/test';
-  var live = 'mongodb://tanepiper:I5qZ7Cw2OqQq@ds061278.mongolab.com:61278/heroku_app21097990';
+  var live = 'mongodb://heroku_app21097990:password@ds061278.mongolab.com:61278/heroku_app21097990';
 
   MongoClient.connect(live, function(err, db) {
 
