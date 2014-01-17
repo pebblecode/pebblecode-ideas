0326893fadc4fc8c7025a2ad21970bda56c507f2
Tane Piper <piper.tane@gmail.com>
2014-01-17 16:39:52 +0000

    YAY! Animation

--- app/public/js/controllers/VoteCtrl.js
+++ app/public/js/controllers/VoteCtrl.js
@@ -5,27 +5,38 @@ angular.module('pebbleidea')
 
     $scope.voteItem = {};
 
+    $scope.voteType = true;
+
     $rootScope.$on('dataLoaded', function() {
       $scope.voteItem = $Ideas.getCurrentVoteItem();
     });
 
     $scope.voteYes = function() {
       // We need to register the vote cast
+      $scope.voteType = 'yes-vote';
       $Vote.castVote({ vote: 'Yes', idea: $scope.voteItem})
       .then(function() {
         $Ideas.nextVoteItem();
         $rootScope.$emit('castVote', { vote: 'Yes', idea: $scope.voteItem});
         $scope.voteItem = $Ideas.getCurrentVoteItem();
+        $timeout(function() {
+          $scope.voteType = '';
+        }, 2000);
+
       });
     }
 
     $scope.voteNo = function() {
       // We need to register the vote cast
+      $scope.voteType = 'no-vote';
       $Vote.castVote({ vote: 'No', idea: $scope.voteItem})
       .then(function() {
         $Ideas.nextVoteItem();
         $rootScope.$emit('castVote', { vote: 'No', idea: $scope.voteItem});
         $scope.voteItem = $Ideas.getCurrentVoteItem();
+        $timeout(function() {
+          $scope.voteType = '';
+        }, 2000);
       });
     }
 
